22dcb6188fc7d3eec1121d0f852abd363625a2c8
TristanRoss <tgr1998@live.com>
2020-08-21 18:37:24 -0500

    Updates

---
+++ precache-manifest.7194fa20741788401f3bc68af477af75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d4de39ef4874042f41ae00d6dc341b",
+    "url": "/react-library/index.html"
+  },
+  {
+    "revision": "f7a12cf4e8656238809d",
+    "url": "/react-library/static/css/main.d1a58d5e.chunk.css"
+  },
+  {
+    "revision": "2c98189dac1ef9092274",
+    "url": "/react-library/static/js/2.940c7109.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-library/static/js/2.940c7109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a12cf4e8656238809d",
+    "url": "/react-library/static/js/main.8956fcb1.chunk.js"
+  },
+  {
+    "revision": "3dbc65b51cf26ec56c5e",
+    "url": "/react-library/static/js/runtime-main.d7fa1cb6.js"
+  }
+]);
